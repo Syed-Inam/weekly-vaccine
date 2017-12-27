37981c9a969b5e39b463f2975b51cada8a5e9dbd
unknown <user@F31322.aku.edu>
2017-12-27 17:29:13 +0500

    final changes

---
+++ Application/login.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Application
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["User"] = null;
+            Login1.Focus();
+        }
+
+        protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+        {
+            Boolean blnresult = false;
+
+            blnresult = Authentication(Login1.UserName, Login1.Password);
+
+            if (blnresult == true)
+            {
+                e.Authenticated = true;
+                Session["User"] = Login1.UserName;
+                Response.Redirect("page.aspx");
+            }
+            else
+                e.Authenticated = false;
+        }
+
+        protected static Boolean Authentication(string username, string password)
+        {
+            string selectcmd = "Select username, password from [UserRegistration] where username='" + username + "' and password ='" + password + "'";
+            string connstring = System.Configuration.ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+
+            SqlConnection conn = new SqlConnection(connstring);
+
+            SqlCommand cmd = new SqlCommand(selectcmd, conn);
+            SqlDataReader reader;
+            conn.Open();
+            try
+            {
+                reader = cmd.ExecuteReader();
+
+
+                if (reader.Read())
+                    return true;
+                else
+                    return false;
+            }
+            finally { conn.Close(); }
+        }
+    }
+}
---
+++ Application/page.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+
+namespace Application
+{
+    public partial class page : System.Web.UI.Page
+    {
+
+         
+
+        string connstring = System.Configuration.ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+        //string connstring = "Data Source=F48604;Initial Catalog=Vaccine; User ID=sa; password=123; Integrated Security=False";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Session["aabb"] = "1";
+
+            txtdss.Focus();            
+            if (Session["User"] != null)
+            {
+                Label1.Text = Convert.ToString(Session["User"]);
+            }
+            else
+                Response.Redirect("login.aspx");
+        }
+
+       
+        protected void submitbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(connstring);
+            conn.Open();
+            try
+            {
+                SqlDataAdapter adpt = new SqlDataAdapter();
+                string SelectStatement = "Select * from Form where Card_No='" + txtcard.Text + "' ";
+                SqlCommand cmd = new SqlCommand(SelectStatement, conn);
+                adpt.SelectCommand = new SqlCommand(SelectStatement, conn);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows == true)
+                {
+                    Response.Redirect("UpdateForm.aspx?ID=" + this.txtdss.Text + "&Card=" + this.txtcard.Text);
+                }
+                else
+                {
+                    Response.Redirect("Form.aspx?ID=" + this.txtdss.Text + "&Card=" + this.txtcard.Text);
+                }
+            }
+            catch (System.Threading.ThreadAbortException) {
+            }
+            catch (SystemException ex)
+            {
+                string msg = "";
+                msg += ex.Message;
+                throw new Exception(msg);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
